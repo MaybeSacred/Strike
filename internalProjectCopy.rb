d2f3085ec160bc37d5625e0fc74c0b9dcc83b92c
MaybeSacred <jtyson3@gatech.edu>
2015-02-08 21:24:12 -0500

    Fixed an error with internalCopying script

--- internalProjectCopy.rb
+++ internalProjectCopy.rb
@@ -2,5 +2,9 @@
 base_dir = Dir.pwd
 $build_dir = build_dir = "C:/Users/Jon/Documents/Github/strike"
 require 'FileUtils'
-
-FileUtils.cp_r(Dir.pwd + "/Assets/Editor Default Resources/.", Dir.pwd + "/Assets/Resources")
+Dir.chdir Dir.pwd + "/Assets/Editor Default Resources"
+vals = Dir.glob("*.prefab")
+for v in vals do
+  v = Dir.pwd + v
+end
+FileUtils.cp_r(vals, $build_dir + "/Assets/Resources")
